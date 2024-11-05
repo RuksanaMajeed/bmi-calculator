64b96a9e967db555f206008ea1a9a4f9caf518e7
Ruksana <ruksanamajeed899@gmail.com>
2024-11-05 19:51:44 +0530

    Working on UI

--- src/Calculator/CalculatorForm.tsx
+++ src/Calculator/CalculatorForm.tsx
@@ -1,60 +1,135 @@
 import React, { useState } from "react";
-
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import { AppBar, Box, FormControlLabel, Switch, Toolbar } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Button,
+  FormControlLabel,
+  Slider,
+  Switch,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import VolunteerActivismIcon from "@mui/icons-material/VolunteerActivism";
-import Typography from "@mui/material/Typography";
-import DarkModeIcon from '@mui/icons-material/DarkMode';
-import LightModeIcon from '@mui/icons-material/LightMode';
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import LightModeIcon from "@mui/icons-material/LightMode";
 
-const CalculatorForm: React.FC<IProps> = (props) => {
+const CalculatorForm: React.FC<IProps> = () => {
   const [isDark, setIsDark] = useState<boolean>(false);
 
   return (
-    <>
-      <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
-        <Box sx={{ flexGrow: 1 }}>
-          <AppBar position="static" >
-            <Toolbar>
-              <VolunteerActivismIcon />
-              <Typography variant="h5" component="div" sx={{ flexGrow: 1}}>
+    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+      <CssBaseline />
+      <Box
+        sx={{
+          flexGrow: 1,
+          minHeight: "100vh",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <AppBar position="static">
+          <Toolbar>
+            <VolunteerActivismIcon />
+            <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
               Health Metrics Tracker
-              </Typography>
-              <FormControlLabel
-                control={<Switch color="default"/>}
-                onClick={() => setIsDark((prev) => !prev)}
-                color="red"
-                // label=""
-                label={
-                    <span style={{ display: 'flex', alignItems: 'center' }}>
-                      {isDark ? <DarkModeIcon style={{ marginRight: 4 }} /> : <LightModeIcon style={{ marginRight: 4 }} />}
-                    </span>
-                  }
-              />
-                {/* {isDark ? (<DarkModeIcon/>) : (<LightModeIcon/>)} */}
-            </Toolbar>
-          </AppBar>
-        </Box>
+            </Typography>
+            <FormControlLabel
+              control={
+                <Switch
+                  color="default"
+                  checked={isDark}
+                  onChange={() => setIsDark((prev) => !prev)}
+                />
+              }
+              label={
+                <span style={{ display: "flex", alignItems: "center" }}>
+                  {isDark ? (
+                    <DarkModeIcon style={{ marginRight: 4 }} />
+                  ) : (
+                    <LightModeIcon style={{ marginRight: 4 }} />
+                  )}
+                </span>
+              }
+            />
+          </Toolbar>
+        </AppBar>
 
-        <CssBaseline />
-        <main>This app is using the {isDark ? "Dark" : "Light"} mode</main>
-      </ThemeProvider>
-    </>
+        <Box
+          sx={{
+            flexGrow: 1,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            background: "background.default",
+            padding: "2rem",
+            minWidth: "500px",
+          }}
+        >
+          <Box
+            sx={{
+              padding: "2rem",
+              borderRadius: "8px",
+              backgroundColor: "background.paper", // Dynamically change background color based on theme
+              color: "text.primary", // Text color based on theme
+              boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.1)",
+              minWidth: "500px",
+              textAlign: "center",
+            }}
+          >
+            <Typography variant="h5" gutterBottom>
+              Calculate BMI
+            </Typography>
+            <Typography variant="h6" align="left">
+              Enter Height
+            </Typography>
+            <Slider valueLabelDisplay="auto" max={300}/>
+            <Typography variant="h6" align="left">
+              Enter Weight
+            </Typography>
+            <Slider valueLabelDisplay="auto" max={200}/>
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+              sx={{ mt: 2 }}
+            >
+              Submit
+            </Button>
+          </Box>
+        </Box>
+      </Box>
+    </ThemeProvider>
   );
 };
 
 interface IProps {}
-export default CalculatorForm;
 
 const darkTheme = createTheme({
   palette: {
     mode: "dark",
+    background: {
+      default: "#121212",
+      paper: "#424242",
+    },
+    text: {
+      primary: "#ffffff",
+    },
   },
 });
 
 const lightTheme = createTheme({
   palette: {
     mode: "light",
+    background: {
+      default: "#f5f5f6",
+      paper: "#ffffff",
+    },
+    text: {
+      primary: "#000000",
+    },
   },
 });
+
+export default CalculatorForm;
